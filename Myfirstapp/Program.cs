b48b3116878def4678870a9051101fd0e4d065c1
Kermo M <melnikovkermo@gmail.com>
2023-02-24 21:20:44 +0200

    create new app

---
+++ Myfirstapp/Program.cs
@@ -0,0 +1,14 @@
+﻿// This is a comment to my code
+
+// Rakendus küsib kasutajalt sisestada tema nime
+// Seejärel rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");//Input
+//string - sõne
+string userName = Console.ReadLine();//Input
+
+Console.WriteLine("Hello, " + userName + "!");//Output
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!");//Output
+
